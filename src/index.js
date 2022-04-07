cee53763f69240e6a199acc911f2aa42ae8e49f4
rlasuddn <rlasuddn>
2022-04-07 18:32:21 +0900

    순한맛 완료

--- src/index.js
+++ src/index.js
@@ -1,10 +1,64 @@
-class Site {}
+class Site {
+    constructor() {
+        this.boards = [];
+    }
 
-class Board {}
+    addBoard(board) {
+        if (this.boards.find((boards) => boards.name === board.name)) {
+            throw new Error();
+        }
+        board.checkboard = true;
+        this.boards.push(board);
+    }
 
-class Article {}
+    findBoardByName(boardname) {
+        return this.boards.find((board) => board.name === boardname);
+    }
+}
 
-class Comment {}
+class Board {
+    constructor(name) {
+        if (name === '' || name === null) throw new Error();
+        this.name = name;
+        this.checkboard = false;
+        this.article = [];
+    }
+    publish(article) {
+        if (this.checkboard === false) throw new Error();
+        article.id = `${this.name}-${Math.random()}`;
+        this.article.push(article);
+        article.createdDate = new Date().toISOString();
+    }
+    getAllArticles() {
+        return this.article;
+    }
+}
+
+class Article {
+    constructor({ subject, content, author }) {
+        if (!subject || !content || !author) {
+            throw new Error();
+        }
+        this.subject = subject;
+        this.content = content;
+        this.author = author;
+        this.comment = [];
+    }
+    reply(comment) {
+        if (!this.id) throw new Error();
+        comment.createdDate = new Date().toISOString();
+        this.comment.push(comment);
+    }
+    getAllComments() {
+        return this.comment;
+    }
+}
+
+class Comment {
+    constructor({ content, author }) {
+        if (!content || !author) throw new Error();
+    }
+}
 
 module.exports = {
     Site,
